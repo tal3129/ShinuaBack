3b8166019100b61ee8cefe729bd1d7852f24850a
Tom Altevet <tom.altevet@gmail.com>
2023-03-21 10:12:47 +0200

    balbla

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,25 @@
 """
 This file handles all the routes (relevant to backend)
-
 Should only route requests from frontend and communicate with backend logic to return PYDANTIC objects
 """
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 from pydantic.dataclasses import dataclass
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    # should return the main frontend page
+    return "HELLO WORLD"
 
 
 @app.post("/catalog")
-def get_item(item: Item): # should be product from db_structs.py WITH db_logic.py
-    return item
+def get_products(): # should be product from db_structs.py WITH db_logic.py
+    return "he" # blablabla
 
 @app.post("/orders")
-def create_item(item: Item): # should be order from db_structs.py WITH db_logic.py
+def get_orders(item: Item): # should be order from db_structs.py WITH db_logic.py
     return item
 
 @app.post("/pickups")
