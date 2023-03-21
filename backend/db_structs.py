d2d80fe5e766e5d65f43447004ba83f9558ec98d
Shoham <bnshoham@gmail.com>
2023-03-21 10:03:55 +0200

    create DB classes

--- backend/db_structs.py
+++ backend/db_structs.py
@@ -1,24 +1,82 @@
 """
-The db contains:
-	Product:
-		- ID
-		- Name
-		- Description
-		- Images(list)
-		- status(in store or not)
-		- Potential amount
-		- Amount in store
+## DB
+Product:
+- id
+- Name
+- Description
+- Image Url List
+- Status
+- Amount
+- Reserved= number of product in orders.
 
-	Product in Pickup/Invitation(could be list of set in the next class):
-		- Product id
-		- current amount
-		
-	Pickup/Invitation:
-		- ID(different from product id)
-		- Place name
-		- Address
-		- Phone number
-		- Email
-		- Description
-		- Product in inivitattion(list)
+Pickup
+- Name
+- Address
+- Date
+- Products: List[ID]
+
+Order:
+- Name
+- Address
+- Date
+- OrderedProducts: List[(ID,Amount)]
+
 """
+from enum import Enum
+from datetime import datetime
+from dataclasses import field 
+from typing import List, Union, Tuple
+from fastapi import FastAPI
+from pydantic.dataclasses import dataclass
+
+
+class ProductStatus(Enum):
+	COLLECTION = 0
+	STORAGE = 1
+
+
+@dataclass
+class Product:
+	pid: int
+    name: str
+    description: str
+    image_url_list: List[str]
+    status: ProductStatus
+    amount: int
+    reserved: int
+
+    def read_from_db():
+    	pass
+
+    def update_to_db():
+    	pass
+
+
+@dataclass
+class Pickup:
+	piid: int
+    name: str
+    address: str
+	date: datetime
+	products: List[int]
+
+    def read_from_db():
+    	pass
+
+    def update_to_db():
+    	pass 
+
+
+@dataclass
+class Order:
+	oid: int
+    name: str
+    address: str
+	date: datetime
+	ordered_products: List[Tuple(int, int)]
+
+    def read_from_db():
+    	pass
+
+    def update_to_db():
+    	pass 
