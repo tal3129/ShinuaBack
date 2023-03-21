ce256c783626d32ae818c517548cdf8daef4e12b
Shoham <bnshoham@gmail.com>
2023-03-21 09:41:12 +0200

    start beckend

---
+++ backend/db_structs.py
@@ -0,0 +1,24 @@
+"""
+The db contains:
+	Product:
+		- ID
+		- Name
+		- Description
+		- Images(list)
+		- status(in store or not)
+		- Potential amount
+		- Amount in store
+
+	Product in Pickup/Invitation(could be list of set in the next class):
+		- Product id
+		- current amount
+		
+	Pickup/Invitation:
+		- ID(different from product id)
+		- Place name
+		- Address
+		- Phone number
+		- Email
+		- Description
+		- Product in inivitattion(list)
+"""
